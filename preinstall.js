077022c47126f7574d425e3bffa89caf7dd1e9c3
Christian Schwarz <christian.schwarz@refactory.at>
2013-11-22 07:24:13 +0100

    Updated preinstall to be only required on windows

--- preinstall.js
+++ preinstall.js
@@ -1,24 +1,13 @@
 var os = require('os');
 var npm = require('npm');
 
-var platform = os.platform();
-
-var package;
-
-if (platform === 'win32')
-    package = 'node-windows';
-else if (platform === 'linux')
-    package = 'node-linux';
-else if (platform === 'darwin')
-    package = 'node-mac';
-else
-    return 'Platform \'' + platform + '\' is not supported';
-
-npm.load({}, function (err) {
-    if (err)
-        return console.log(err);
-    npm.commands.install([package], function(err) {
+if (os.platform() === 'win32') {
+    npm.load({}, function (err) {
         if (err)
-            console.log(err);
+            return console.log(err);
+        npm.commands.install(['node-windows'], function(err) {
+            if (err)
+                console.log(err);
+        });
     });
-});
+}
